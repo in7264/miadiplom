95cc24d8731f5e8382ccde4c66bd6400fa93b964
Ivan Nimych <in7264@gmail.com>
2023-11-19 21:56:43 +0200

    1.4v

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const PORT = 3000;
 // Подключение к MongoDB
 mongoose.connect('mongodb+srv://in7264:gfhjkm7264@nyotvali0.x8murab.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true });
 
+// Создание схемы для пользователя
 const userSchema = new mongoose.Schema({
     username: String,
     password: String
@@ -16,7 +17,15 @@ const userSchema = new mongoose.Schema({
   
   const User = mongoose.model('User', userSchema);
   
-  // Use bodyParser middleware
+  // Добавление middleware для обработки CORS
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Разрешает запросы от всех источников
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+  });
+  
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
   
@@ -33,41 +42,41 @@ const userSchema = new mongoose.Schema({
   
   app.post('/register', async (req, res) => {
     try {
-      const { username, password } = req.body; // Ensure req.body is properly parsed
+      const { username, password } = req.body;
   
       const existingUser = await User.findOne({ username });
   
       if (existingUser) {
-        return res.status(409).json({ message: 'User with this username already exists' });
+        return res.status(409).json({ message: 'Пользователь с таким именем уже существует' });
       }
   
       const newUser = new User({ username, password });
       await newUser.save();
   
-      res.status(201).json({ message: 'User successfully registered' });
+      res.status(201).json({ message: 'Пользователь успешно зарегистрирован' });
     } catch (error) {
-      console.error('Error during registration:', error);
-      res.status(500).json({ message: 'Server error during registration' });
+      console.error('Ошибка во время регистрации:', error);
+      res.status(500).json({ message: 'Ошибка сервера во время регистрации' });
     }
   });
   
   app.post('/login', async (req, res) => {
     try {
-      const { username, password } = req.body; // Ensure req.body is properly parsed
+      const { username, password } = req.body;
   
       const user = await User.findOne({ username, password });
   
       if (user) {
-        res.status(200).json({ message: 'Login successful' });
+        res.status(200).json({ message: 'Авторизация успешна' });
       } else {
-        res.status(401).json({ message: 'Incorrect username or password' });
+        res.status(401).json({ message: 'Неверный логин или пароль' });
       }
     } catch (error) {
-      console.error('Error during login:', error);
-      res.status(500).json({ message: 'Server error during login' });
+      console.error('Ошибка во время входа:', error);
+      res.status(500).json({ message: 'Ошибка сервера во время входа' });
     }
   });
   
   app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Сервер запущен на порту ${PORT}`);
   });

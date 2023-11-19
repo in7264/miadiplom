ee8f147abd7cbf68a1f1b70e5b6293d22379c817
Ivan Nimych <in7264@gmail.com>
2023-11-19 21:41:42 +0200

    1.1v

--- public/script.js
+++ public/script.js
@@ -1,27 +1,60 @@
-document.getElementById('loginForm').addEventListener('submit', async function(event) {
+document.addEventListener('DOMContentLoaded', () => {
+  const registerForm = document.getElementById('registerForm');
+  const loginForm = document.getElementById('loginForm');
+
+  registerForm.addEventListener('submit', async (event) => {
     event.preventDefault();
-  
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-  
+
+    const formData = new FormData(registerForm);
+    const data = {
+      username: formData.get('username'),
+      password: formData.get('password')
+    };
+
     try {
-      const response = await fetch('http://localhost:3000/login', {
+      const response = await fetch('/register', {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json',
+          'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ username, password }),
+        body: JSON.stringify(data)
       });
-  
-      const data = await response.json();
-  
-      if (response.ok) {
-        alert(data.message);
-      } else {
-        alert(data.message);
-      }
+
+      const result = await response.json();
+      console.log(result.message);
+
+      // Optionally, you can redirect to another page after successful registration
+      // window.location.href = '/success'; // Change '/success' to the desired URL
     } catch (error) {
-      console.error('Ошибка при отправке запроса:', error);
+      console.error('Error during registration:', error.message);
     }
   });
-  
+
+  loginForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(loginForm);
+    const data = {
+      username: formData.get('username'),
+      password: formData.get('password')
+    };
+
+    try {
+      const response = await fetch('/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      });
+
+      const result = await response.json();
+      console.log(result.message);
+
+      // Optionally, you can redirect to another page after successful login
+      // window.location.href = '/dashboard'; // Change '/dashboard' to the desired URL
+    } catch (error) {
+      console.error('Error during login:', error.message);
+    }
+  });
+});
--- server.js
+++ server.js
@@ -16,40 +16,58 @@ const userSchema = new mongoose.Schema({
   
   const User = mongoose.model('User', userSchema);
   
+  // Use bodyParser middleware
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
   
-  // Указываем папку со статическими файлами (CSS, изображения, скрипты)
-  app.use(express.static(path.join(__dirname, 'public')));
+  // Serve static files
+  app.use('/public', express.static(path.join(__dirname, 'public')));
   
-  // Отправка HTML страницы при обращении к корневому пути
   app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
   });
   
-  // Обработка GET-запроса на /login
   app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
   });
   
-  // Обработка запросов на авторизацию
-  app.post('/login', async (req, res) => {
-    const { username, password } = req.body;
-  
+  app.post('/register', async (req, res) => {
     try {
-      const user = await User.findOne({ username, password });
+      const { username, password } = req.body; // Ensure req.body is properly parsed
   
-      if (user) {
-        res.status(200).json({ message: 'Авторизация успешна' });
-      } else {
-        res.status(401).json({ message: 'Неверный логин или пароль' });
+      const existingUser = await User.findOne({ username });
+  
+      if (existingUser) {
+        return res.status(409).json({ message: 'User with this username already exists' });
       }
+  
+      const newUser = new User({ username, password });
+      await newUser.save();
+  
+      res.status(201).json({ message: 'User successfully registered' });
     } catch (error) {
-      res.status(500).json({ message: 'Ошибка сервера' });
+      console.error('Error during registration:', error);
+      res.status(500).json({ message: 'Server error during registration' });
     }
   });
   
-  // Запуск сервера
+  app.post('/login', async (req, res) => {
+    try {
+      const { username, password } = req.body; // Ensure req.body is properly parsed
+  
+      const user = await User.findOne({ username, password });
+  
+      if (user) {
+        res.status(200).json({ message: 'Login successful' });
+      } else {
+        res.status(401).json({ message: 'Incorrect username or password' });
+      }
+    } catch (error) {
+      console.error('Error during login:', error);
+      res.status(500).json({ message: 'Server error during login' });
+    }
+  });
+  
   app.listen(PORT, () => {
-    console.log(`Сервер запущен на порту ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
   });

9575ec0b48cf861586457f2fd4a5abe0588464cc
Ivan Nimych <in7264@gmail.com>
2023-11-20 20:09:26 +0200

    1.00v

---
+++ script.js
@@ -0,0 +1,80 @@
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+function login(event) {
+    event.preventDefault();
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    // Реализуйте логику входа на стороне клиента
+    console.log("Вход выполнен с логином: " + username);
+}
+
+function register(event) {
+    event.preventDefault();
+    var newUsername = document.getElementById("newUsername").value;
+    var newPassword = document.getElementById("newPassword").value;
+    // Реализуйте логику регистрации на стороне клиента
+    console.log("Пользователь зарегистрирован с логином: " + newUsername);
+    // Сохраните данные в локальном хранилище или другом месте по вашему выбору
+    saveUserDataLocally(newUsername, newPassword);
+    // Отправите данные на GitHub (замените на свои данные)
+    sendToGitHub(newUsername, newPassword);
+}
+
+function saveUserDataLocally(username, password) {
+    // Создаем объект с данными пользователя
+    const userData = {
+        username: username,
+        password: password,
+    };
+
+    // Конвертируем объект в строку JSON
+    const userDataString = JSON.stringify(userData);
+
+    // Сохраняем строку JSON в localStorage
+    localStorage.setItem('userData', userDataString);
+
+    console.log('Данные пользователя успешно сохранены локально.');
+}
+
+
+function sendToGitHub(username, password) {
+    // Это пример. Не храните токен на стороне клиента в реальном приложении!
+    const githubToken = 'ВАШ_ТОКЕН';
+
+    const data = {
+        username: username,
+        password: password,
+    };
+
+    fetch('https://api.github.com/repos/in7264/miadiplom/contents/users.json', {
+        method: 'PUT',
+        headers: {
+            'Authorization': `Bearer ${githubToken}`,
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            message: 'Добавление данных пользователя',
+            content: btoa(JSON.stringify(data)),
+        }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Данные успешно отправлены на GitHub:', data);
+    })
+    .catch(error => {
+        console.error('Ошибка при отправке данных на GitHub:', error);
+    });
+}
+
